1abf86be92a11a8f4fda511ff64ead9e97829e70
Bigya <bigyadhungana@gmail.com>
2022-11-24 23:10:07 +0545

    ui updated again

--- Bhitti/employeedash.cpp
+++ Bhitti/employeedash.cpp
@@ -4,6 +4,7 @@
 #include "trackorder.h"
 #include "viewproduct.h"
 #include "cancelorder.h"
+#include "login.h"
 
 EmployeeDash::EmployeeDash(QWidget *parent) :
     QDialog(parent),
@@ -21,7 +22,7 @@ void EmployeeDash::on_pushButton_clicked()
 {
     this->hide();
     NewOrder *o=new NewOrder;
-    o->showMaximized();
+    o->show();
 }
 
 
@@ -29,22 +30,32 @@ void EmployeeDash::on_pushButton_2_clicked()
 {
     this->hide();
     TrackOrder *t=new TrackOrder;
-    t->showMaximized();
+    t->show();
 }
 
 
 void EmployeeDash::on_pushButton_3_clicked()
 {
     this->hide();
-    ViewProduct *v=new ViewProduct;
-    v->showMaximized();
+    CancelOrder *c=new CancelOrder;
+    c->show();
+
 }
 
 
 void EmployeeDash::on_pushButton_4_clicked()
 {
     this->hide();
-    CancelOrder *c=new CancelOrder;
-    c->showMaximized();
+    ViewProduct *v=new ViewProduct;
+    v->show();
+}
+
+
+void EmployeeDash::on_pushButton_6_clicked()
+{
+    this->hide();
+    Login *newentry=new Login;
+    newentry->show();
+
 }
 
--- Bhitti/main.cpp
+++ Bhitti/main.cpp
@@ -13,22 +13,22 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     //Login w;
     //w.show();
-    //EmployeeDash emp1;
-    //emp1.showMaximized();
-    NewOrder o1;
-    o1.show();
-    /*TrackOrder order;
-    order.showMaximized();
-    ViewProduct product1;
-    product1.showMaximized();
-    CancelOrder o11;
-    o11.showMaximized();
+    EmployeeDash emp1;
+    emp1.showMaximized();
+    //NewOrder o1;
+    //o1.show();
+   // TrackOrder order;
+    //order.show();
+   // ViewProduct product1;
+    //product1.show();
+   //CancelOrder o11;
+    //o11.showMaximized();
 
-    Attendance atten;
-    atten.showMaximized();
-    NewProduct np1;
-    np1.showMaximized();
-*/
+   // Attendance atten;
+    //atten.show();
+    //NewProduct np1;
+    //np1.show();
+ /**/
     //AdminDash aa;
     //aa.showMaximized();
     return a.exec();
